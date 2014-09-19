2c0865240239232417466caa8958eccfa0e4ac48
pbuholzer <patrik.buholzer@stud.hslu.ch>
2014-09-19 11:12:00 +0200

    HelloWorld

---
+++ CH.HSLU.PREN.Evaluation/src/ch/hslu/pren/evaluation/Evaluation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.hslu.pren.evaluation;
+
+/**
+ *
+ * @author Patrik
+ */
+public class Evaluation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HelloWorld hw = new HelloWorld();
+        hw.writeToConsole();
+    }
+    
+}
---
+++ CH.HSLU.PREN.Evaluation/src/ch/hslu/pren/evaluation/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.hslu.pren.evaluation;
+
+/**
+ *
+ * @author Patrik
+ */
+public class HelloWorld {
+    
+    public HelloWorld() {
+    
+    }
+    
+    public void writeToConsole() {
+        System.out.println("Hello World");
+    }
+    
+}
